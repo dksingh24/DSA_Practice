9628abefab6545d3fd12067491385459cab0f1e7
dksingh24 <dushyant.mca22.du@gmail.com>
2026-03-08 21:04:00 +0530

    Updated BST Code

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -1,173 +1,193 @@
-package BinarySearchTree;
-
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.Queue;
 
 public class BST {
 
-    private static class TreeNode {
+    private static class TreeNode{
         int data;
         TreeNode left;
         TreeNode right;
 
-        TreeNode(int data) {
+        TreeNode(){}
+
+        TreeNode(int data){
             this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
 
     private TreeNode root;
 
-    /* =========================================================
-       INSERT
-       ========================================================= */
-
-    public void insert(int val) {
-        root = insert(root, val);
+    public void insert(int val){
+        root = insert(this.root, val);
     }
 
-    private TreeNode insert(TreeNode root, int val) {
-        if (root == null)
-            return new TreeNode(val);
-
-        if (val < root.data)
-            root.left = insert(root.left, val);
-        else if (val > root.data)
-            root.right = insert(root.right, val);
-
-        return root; // ignore duplicates
-    }
-
-    /* =========================================================
-       SEARCH
-       ========================================================= */
-
-    public boolean isPresent(int val) {
-        return searchRecursive(root, val) != null;
-    }
-
-    private TreeNode searchRecursive(TreeNode root, int val) {
-        if (root == null || root.data == val)
+    private TreeNode insert(TreeNode root, int val){
+        if(root == null){
+            root = new TreeNode(val);
             return root;
-
-        if (val < root.data)
-            return searchRecursive(root.left, val);
-        else
-            return searchRecursive(root.right, val);
-    }
-
-    private TreeNode searchIterative(int val) {
-        TreeNode temp = root;
-
-        while (temp != null) {
-            if (temp.data == val)
-                return temp;
-
-            temp = val < temp.data ? temp.left : temp.right;
         }
-
-        return null;
-    }
-
-    /* =========================================================
-       FIND MIN
-       ========================================================= */
-
-    public int findMinValue() {
-        if (root == null)
-            throw new NoSuchElementException("Tree is empty");
-
-        return findMinNode(root).data;
-    }
-
-    private TreeNode findMinNode(TreeNode node) {
-        while (node.left != null)
-            node = node.left;
-
-        return node;
-    }
-
-    /* =========================================================
-       FIND MAX
-       ========================================================= */
-
-    public int findMaxValue() {
-        if (root == null)
-            throw new NoSuchElementException("Tree is empty");
-
-        return findMaxNode(root).data;
-    }
-
-    private TreeNode findMaxNode(TreeNode node) {
-        while (node.right != null)
-            node = node.right;
-
-        return node;
-    }
-
-    /* =========================================================
-       DELETE
-       ========================================================= */
-
-    public void delete(int val) {
-        if (!isPresent(val))
-            throw new IllegalStateException("Value not present in BST");
-
-        root = delete(root, val);
-    }
-
-    private TreeNode delete(TreeNode root, int val) {
-        if (root == null)
-            return null;
-
-        if (val < root.data) {
-            root.left = delete(root.left, val);
-        } else if (val > root.data) {
-            root.right = delete(root.right, val);
-        } else {
-
-            // Case 1: No child
-            if (root.left == null && root.right == null)
-                return null;
-
-            // Case 2: One child
-            if (root.left == null)
-                return root.right;
-
-            if (root.right == null)
-                return root.left;
-
-            // Case 3: Two children
-            TreeNode successor = findMinNode(root.right);
-            root.data = successor.data;
-            root.right = delete(root.right, successor.data);
-        }
-
+        if(root.data > val)
+            root.left = insert(root.left, val);
+        else if(root.data < val)
+            root.right = insert(root.right, val);
         return root;
     }
 
-    /* =========================================================
-       HEIGHT (Number of Levels / Nodes Based)
-       ========================================================= */
-
-    public int height() {
+    public int height(){
         return height(root);
     }
 
-    private int height(TreeNode root) {
-        if (root == null)
+    private int height(TreeNode root){
+        if(root == null)
             return 0;
-
         return 1 + Math.max(height(root.left), height(root.right));
     }
 
-    public void printBST() {
+    
+
+    // region Delete Node
+    public void delete(int val) {
+        if(root == null)
+            throw new IllegalStateException("Tree is Empty");
+        root = delete(root, val);
+    }
+
+    private TreeNode delete(TreeNode root, int val){
+        if(root == null)
+            throw new IllegalStateException("The give node value is not present in BST.");
+        if(root.data > val)
+            root.left = delete(root.left, val);
+        else if(root.data < val)
+            root.right = delete(root.right, val);
+        else{
+            if(root.left == null && root.right == null)
+                return null;
+            else if(root.left == null)
+                return root.right;
+            else if(root.right == null)
+                return root.left;
+            else{
+                TreeNode successor = findMinNode(root.right);
+                root.data = successor.data;
+                root.right = delete(root.right, successor.data);
+            }
+        }
+        return root;
+    }
+    // endregion
+
+    // region Find Maximum node and its value
+    /**
+     * Finds the value of the maximum value node of BST.
+     * @return The integer(int) value of the maximum value node.
+     * @throws  NoSuchElementException in case the Tree is Empty.
+     */
+    public int findMaxValue(){
+        TreeNode max = findMaxNode();
+        if(max != null)
+            return max.data;
+        throw new NoSuchElementException("The Tree is Empty");
+    }
+
+    /**
+     * Finds the maximum value node of BST.
+     * @return The maximum value node of BST.
+     */
+    private TreeNode findMaxNode(){
+        if(root == null)
+            return null;
+        TreeNode temp = root;
+        while(temp.right != null)
+            temp = temp.right;
+        return temp;
+    }
+    // endregion
+
+    // region Find Minimum Node and its value
+    /**
+     * Finds the value of the minimum value node of BST
+     * @return The integer(int) value of the minimum node of BST
+     */
+    public int findMinValue(){
+        TreeNode min = findMinNode();
+        if(min != null)
+            return min.data;
+        throw new NoSuchElementException("The Tree is empty");
+    }
+    
+    /**
+     * Find the minimum node of BST
+     * @return The minimum node of BST
+     */
+    private TreeNode findMinNode(){
+        if(root == null)
+            return null;
+        TreeNode temp = root;
+        while(temp.left != null)
+            temp = temp.left;
+        return temp;
+    }
+
+    private TreeNode findMinNode(TreeNode node){
+        if(node == null)
+            return null;
+        while(node.left != null)
+            node = node.left;
+        return node;
+    }
+    // endregion
+    
+    //region Search Methods
+    public boolean isPresent(int val){
+        return search(val) != null;
+    }
+
+    /**
+     * Recursive Search Method
+     */
+    private TreeNode search(int val){
+        // return searchHelper(root, val);
+        return searchIterative(val);
+    }
+
+    /**  
+     * Recursive Search Helper Method
+     */
+    private TreeNode searchHelper(TreeNode root, int val){
+        if(root == null || root.data == val)
+            return root;
+        if(root.data > val)
+            return searchHelper(root.left, val);
+        else if(root.data < val)
+            return searchHelper(root.right, val);
+        return null;
+    }
+
+    /**
+    * Iterative Search
+    */
+    private TreeNode searchIterative(int val){
+        TreeNode temp = root;
+        while(temp != null){
+            if(temp.data == val)
+                return temp;
+            temp = temp.data > val ? temp.left : temp.right;
+        }
+        return null;
+    }
+    // endregion
+
+    public void printsBST() {
         if (root == null) {
             System.out.println("BST is Empty!");
             return;
         }
 
-        System.out.println("BST Structure (Level Order):");
+        System.out.println("BST Status:");
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
@@ -175,15 +195,13 @@ public class BST {
         boolean hasNonNull = true;
 
         while (hasNonNull) {
-
             int size = queue.size();
             hasNonNull = false;
 
             for (int i = 0; i < size; i++) {
-
                 TreeNode node = queue.poll();
 
-                if (node == null) {
+                if (node == null) { 
                     System.out.print("null ");
                     queue.offer(null);
                     queue.offer(null);
@@ -192,12 +210,38 @@ public class BST {
                     queue.offer(node.left);
                     queue.offer(node.right);
 
-                    if (node.left != null || node.right != null)
+                    if (node.left != null || node.right != null) {
                         hasNonNull = true;
+                    }
                 }
             }
         }
+    }
 
-        System.out.println();
+    public void printBST(){
+        if(root == null){
+            System.out.println("BST is Empty!");
+            return;
+        }
+        System.out.println("BST Status :");
+        Queue<TreeNode> pendingNodes = new LinkedList<>();
+        pendingNodes.offer(root);
+        while(!pendingNodes.isEmpty()){
+            int size = pendingNodes.size();
+            
+            while(size-- > 0){
+                TreeNode node = pendingNodes.poll();
+                if(node != null)
+                    System.out.print(node.data + " ");
+                else{
+                    System.out.print("null ");
+                    break;
+                }
+                
+                pendingNodes.offer(node.left);
+                pendingNodes.offer(node.right);
+                
+            }
+        }
     }
 }
--- BinarySearchTree/BSTImplementation.java
+++ BinarySearchTree/BSTImplementation.java
@@ -1,12 +1,28 @@
-package BinarySearchTree;
+import java.util.NoSuchElementException;
 
 public class BSTImplementation {
     public static void main(String[] args) {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-
         BST bst = new BST();
+        bst.insert(5);
+        bst.insert(2);
+        bst.insert(6);
+        bst.insert(0);
+        bst.insert(1);
+        bst.insert(3);
+        bst.insert(8);
+        // bst.insert(10);
+        bst.insert(7);
+        // bst.insert(9);
+        // bst.printBST();
+        // try{
+        //     bst.delete(9);
+        // } catch (Exception nse){
+        //     System.out.println(nse.getMessage());
+        // }
+        System.out.println(bst.height());
+        System.out.println();
         bst.printBST();
     }
 }
-
